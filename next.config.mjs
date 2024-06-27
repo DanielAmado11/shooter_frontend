d464ebfd77aefa18f7f18b15e358b8657a0b2be5
Carlos González <carlosj.gonzalez28@gmail.com>
2024-06-27 10:52:01 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   env: {
     PUBLIC_API_URL: "https://xdmocaadfutbolgameapi.azurewebsites.net",
-    // PUBLIC_API_URL: "http://localhost:3001",
   },
   reactStrictMode: true,
   images: {
