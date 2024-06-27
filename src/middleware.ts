82b24e52a894cb9702efe81480d132af1a84aea7
Carlos González <carlosj.gonzalez28@gmail.com>
2024-06-27 17:10:40 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = (req: NextRequest, res: NextResponse) => {
-  if (req.cookies.get("user")) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  } else {
-    return NextResponse.redirect(new URL("/home", req.url));
-  }
+  // if (req.cookies.get("user")) {
+  //   return NextResponse.redirect(new URL("/dashboard", req.url));
+  // } else {
+  //   return NextResponse.redirect(new URL("/home", req.url));
+  // }
 };
 
 export const config = {
