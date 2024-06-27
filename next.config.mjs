abc587466a97d6f916861b84794f10c800f0a8f7
Carlos González <carlosj.gonzalez28@gmail.com>
2024-06-27 10:01:32 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    // PUBLIC_API_URL: "https://xdmocaadfutbolgameapi.azurewebsites.net",
+    PUBLIC_API_URL: "http://localhost:3001",
+  },
+  reactStrictMode: true,
+  images: {
+    loader: "custom",
+  },
+  // output: "export",
+};
+
+export default nextConfig;
