16006e4d3581898ed2e5f8d7e71bc50faa1be61a
DanielAmado11 <danielamado1107@gmail.com>
2024-07-11 16:08:06 -0500

    add user code to cookies

--- src/config/clientAxiosInstance.js
+++ src/config/clientAxiosInstance.js
@@ -1,9 +1,16 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const getClientAxiosInstance = () => {
+  const user_code = Cookies.get("user_code");
   const instance = axios.create({
     baseURL: process.env.PUBLIC_API_URL,
     withCredentials: true,
+    // add cookie user_code to all requests
+    headers: {
+      "Content-Type": "application/json",
+      user_code: user_code,
+    },
   });
 
   return instance;
