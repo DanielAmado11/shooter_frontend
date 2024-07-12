d49617514e883efa4b9bac02aa37f47d00b56618
DanielAmado11 <danielamado1107@gmail.com>
2024-07-12 17:37:58 -0500

    change sounds

--- src/components/sounds/sounds.jsx
+++ src/components/sounds/sounds.jsx
@@ -12,52 +12,67 @@ export const sounds = {
     },
   }),
   background_2: new Howl({
-    src: ["/sounds/loop_2.ogg"],
+    src: ["/sounds/loop_2.mp3"],
     volume: 0.5,
     loop: true,
+    onplayerror: function () {
+      background_2.once("unlock", function () {
+        background_2.play();
+      });
+    },
   }),
   stadium: new Howl({
-    src: ["/sounds/tribuna.ogg"],
+    src: ["/sounds/tribuna.mp3"],
     volume: 0.5,
     loop: true,
+    onplayerror: function () {
+      stadium.once("unlock", function () {
+        stadium.play();
+      });
+    },
   }),
   kick_1: new Howl({
-    src: ["/sounds/Balon_1.ogg"],
+    src: ["/sounds/Balon_1.mp3"],
     volume: 0.5,
+    onplayerror: function () {
+      kick_1.once("unlock", function () {
+        kick_1.play();
+      });
+    },
   }),
   kick_2: new Howl({
-    src: ["/sounds/Balon_2.ogg"],
+    src: ["/sounds/Balon_2.mp3"],
     volume: 0.5,
   }),
   kick_3: new Howl({
-    src: ["/sounds/Balon_3.ogg"],
+    src: ["/sounds/Balon_3.mp3"],
     volume: 0.5,
   }),
   keep_1: new Howl({
-    src: ["/sounds/Arquero_1.ogg"],
+    src: ["/sounds/Arquero_1.mp3"],
     volume: 0.5,
   }),
   keep_2: new Howl({
-    src: ["/sounds/Arquero_2.ogg"],
+    src: ["/sounds/Arquero_2.mp3"],
     volume: 0.5,
   }),
   keep_3: new Howl({
-    src: ["/sounds/Arquero_3.ogg"],
+    src: ["/sounds/Arquero_3.mp3"],
     volume: 0.5,
   }),
   goal: new Howl({
-    src: ["/sounds/Gol.ogg"],
+    src: ["/sounds/Gol.mp3"],
     volume: 0.5,
   }),
   goal_stadium: new Howl({
-    src: ["/sounds/tribuna_gol.ogg"],
+    src: ["/sounds/tribuna_gol.mp3"],
   }),
   whistle_1: new Howl({
-    src: ["/sounds/Silbato_1.ogg"],
+    src: ["/sounds/Silbato_1.mp3"],
     volume: 0.5,
   }),
   whistle_2: new Howl({
-    src: ["/sounds/Silbato_2.ogg"],
+    src: ["/sounds/Silbato_2.mp3"],
     volume: 0.5,
   }),
 };
