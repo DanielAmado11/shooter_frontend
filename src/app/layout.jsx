eaabb4ab9491763acd283b3537e0369b4b01acd6
DanielAmado11 <danielamado1107@gmail.com>
2024-07-12 16:26:04 -0500

    ..

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,32 +19,47 @@ const poppins = Poppins({
 
 const queryClient = new QueryClient();
 
+const isIOS = () => {
+  return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+};
+
 export default function RootLayout({ children }) {
   const pathname = usePathname();
   const [rotate, setRotate] = useState(false);
   const [soundsInitialized, setSoundsInitialized] = useState(false);
 
   useEffect(() => {
+    const initializeSounds = () => {
+      if (!soundsInitialized) {
+        sounds.background_1.play();
+        setSoundsInitialized(true);
+      }
+    };
+
+    if (isIOS()) {
+      document.addEventListener("click", initializeSounds, { once: true });
+      document.addEventListener("touchstart", initializeSounds, { once: true });
+    } else {
+      initializeSounds();
+    }
+
     function handleResize() {
-      const isMobile = window.innerWidth <= 768; // Puedes ajustar el ancho según tus necesidades
+      const isMobile = window.innerWidth <= 768;
       const isVertical = window.innerHeight > window.innerWidth;
       setRotate(isMobile && isVertical);
     }
 
-    // Agregar el evento de resize
     window.addEventListener("resize", handleResize);
-
-    // Verificar la orientación inicial
     handleResize();
 
-    // Limpiar el evento de resize
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const initializeSounds = () => {
-    sounds.background_1.play();
-    setSoundsInitialized(true);
-  };
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (isIOS()) {
+        document.removeEventListener("click", initializeSounds);
+        document.removeEventListener("touchstart", initializeSounds);
+      }
+    };
+  }, [soundsInitialized]);
 
   return (
     <html lang="en">
@@ -52,26 +67,6 @@ export default function RootLayout({ children }) {
         <QueryClientProvider client={queryClient}>
           <AuthProvider getAuth={getUser}>
             <Rotate open={rotate} onClose={() => setRotate(false)} />
-            {!soundsInitialized && (
-              <button
-                style={{
-                  position: "absolute",
-                  top: "90%",
-                  left: "10%",
-                  transform: "translate(-50%, -50%)",
-                  padding: "20px",
-                  fontSize: "20px",
-                  background: "white",
-                  border: "none",
-                  borderRadius: "10px",
-                  cursor: "pointer",
-                }}
-                onClick={initializeSounds}
-                onTouchStart={initializeSounds}
-              >
-                Start Sound
-              </button>
-            )}
             <Suspense fallback={<Loader />}>{children}</Suspense>
           </AuthProvider>
         </QueryClientProvider>
