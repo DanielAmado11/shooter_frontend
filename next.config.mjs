fd1e21cd9cf7926af6029f9d119b8f129143f6d5
DanielAmado11 <danielamado1107@gmail.com>
2024-08-06 10:44:44 -0500

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    PUBLIC_API_URL: process.env.PUBLIC_API_URL,
-    PUBLIC_URL: process.env.PUBLIC_URL,
-    // PUBLIC_API_URL: "https://shooter-frontend-zeta.vercel.app/",
-    // PUBLIC_API_URL: "https://xdmocaadfutbolgameapi.azurewebsites.net",
+    // PUBLIC_API_URL: process.env.PUBLIC_API_URL,
+    // PUBLIC_URL: process.env.PUBLIC_URL,
+    PUBLIC_API_URL: "https://shooter-frontend-zeta.vercel.app/",
+    PUBLIC_API_URL: "https://xdmocaadfutbolgameapi.azurewebsites.net",
     // TIMER: process.env.TIMER,
   },
   reactStrictMode: true,
